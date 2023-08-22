194e9ef4352f4eea9bcf485ac807bc74fb489b48
SeokYeong Jeong <aswe0409@gmail.com>
2023-08-22 16:01:11 +0900

    백준

---
+++ 7568.py
@@ -0,0 +1,11 @@
+num = int(input())
+
+people = [list(map(int,input().split())) for i in range(num)]
+
+for i in range(num):
+    cnt = 1
+    for j in range(num):
+        if(people[i][0] < people[j][0] and people[i][1] < people[j][1]):
+            cnt +=1
+    print(cnt,end= " ")
+    
